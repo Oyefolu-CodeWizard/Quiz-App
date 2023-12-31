bd6bccd8a1a20550c171b227931b3fb07ce8e92a
CODE_WIZARD <oyefolutoyyib16@gmail.com>
2023-12-31 01:58:27 +0100

    second 10 questions

--- app.js
+++ app.js
@@ -228,6 +228,227 @@ const questions = [
       },
     ],
   },
+
+  {
+    question: "What is Hoisting in javascript?",
+    answer: [
+      {
+        text: "Hoisting is the behaviour of javascript where all Object are stored in the heap.",
+        correct: false,
+      },
+      {
+        text: `Hoisting is the default behaviour of javascript where all the variable and function declarations are moved on top.
+        This means that irrespective of where the variables and functions are declared, they are moved on top of the scope. The scope can be both local and global.`,
+        correct: true,
+      },
+      {
+        text: "Hoisting is the default behaviour of javascript where all the variable and function declarations are stored in the call stack.",
+        correct: false,
+      },
+      {
+        text: "Hoisting is the default behaviour of javascript where all arrays are moved below the scope.",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    question: "What is the difference between “ == “ and “ === “ operators?",
+    answer: [
+      {
+        text: "Both are comparison operators. The difference between both the operators is that “==” is used to compare values whereas, “ === “ is used to compare both values and types.",
+        correct: true,
+      },
+      {
+        text: "The difference between both the operators is that “==” is an equality sign whereas, “ === “ is a comparison sign.",
+        correct: false,
+      },
+      {
+        text: "The difference between both the operators is that “==” is an assignment operator whereas, “ === “ is a conditional operator.",
+        correct: false,
+      },
+      {
+        text: "The difference between both the operators is that “==” is used to compare variables whereas, “ === “ is used to compare both variables and functions.",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    question: "What is Type Coercion in javascript?",
+    answer: [
+      {
+        text: "Type coercion in javascript is the conversion of strings to numbers.",
+        correct: false,
+      },
+      {
+        text: "Type coercion is the automatic conversion of number to strings.",
+        correct: false,
+      },
+      {
+        text: "Type coercion in javascript is the automatic conversion of value from one data type to another. It takes place when the operands of an expression are of different data types.",
+        correct: true,
+      },
+      {
+        text: "Type coercion in javascript is the automatic conversion of numbers from one data type to another.",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    question: "What is NaN property in JavaScript?",
+    answer: [
+      {
+        text: "NaN indicates a value that is not a legal string.",
+        correct: false,
+      },
+      {
+        text: "NaN indicates a value that is a string.",
+        correct: false,
+      },
+      {
+        text: "NaN indicates a value that is a legal number.",
+        correct: false,
+      },
+      {
+        text: "NaN property represents the “Not-a-Number” value. It indicates a value that is not a legal number.",
+        correct: true,
+      },
+    ],
+  },
+
+  {
+    question: "What is an Immediately Invoked Function in JavaScript?",
+    answer: [
+      {
+        text: "An Immediately Invoked Function ( known as IIFE and pronounced as IIFY) is a function that runs as soon as it is defined.",
+        correct: true,
+      },
+      {
+        text: "It is a function that runs as soon as it is called.",
+        correct: false,
+      },
+      {
+        text: "It is an asynchronous function that runs as soon as it is defined.",
+        correct: false,
+      },
+      {
+        text: "It is a function that runs as soon as it is returned.",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    question: "What is “this” keyword?",
+    answer: [
+      {
+        text: "The “this” keyword refers to the event that the function is a property of.",
+        correct: false,
+      },
+      {
+        text: "The “this” keyword refers to the function that the parameter is a property of.",
+        correct: false,
+      },
+      {
+        text: "The “this” keyword refers to the object that the function is a property of.",
+        correct: true,
+      },
+      {
+        text: "The “this” keyword refers to the object that the value is a property of.",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    question: "What is currying in JavaScript?",
+    answer: [
+      {
+        text: "Currying is an advanced technique to transform a function of arguments n, to n functions of one or fewer arguments.",
+        correct: true,
+      },
+      {
+        text: "Currying is an advanced technique to transform a function of parenthesis n, to n functions of one or fewer arguments.",
+        correct: false,
+      },
+      {
+        text: "Currying is an advanced technique to transform a function of arguments n, to n functions of one or fewer parameter.",
+        correct: false,
+      },
+      {
+        text: "Currying is an advanced technique to transform a function of parenthesis n, to n functions of one or fewer parameter.",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    question: "What is Closures in JavaScript?",
+    answer: [
+      {
+        text: "Closures are an ability of a function to remember it's parameter and arguments that are declared in its inner scope.",
+        correct: false,
+      },
+      {
+        text: "Closures are an ability of a function to remember the variables and functions that are declared in its inner scope.",
+        correct: false,
+      },
+      {
+        text: "Closures are an ability of an object to remember the property and value that are declared in its outer scope.",
+        correct: false,
+      },
+      {
+        text: "Closures are an ability of a function to remember the variables and functions that are declared in its outer scope.",
+        correct: true,
+      },
+    ],
+  },
+
+  {
+    question: "What are callbacks?",
+    answer: [
+      {
+        text: "A callback is a function that will be executed after another function gets executed. In javascript, functions are treated as first-class citizens, they can be used as an argument of another function, can be returned by another function, and can be used as a property of an object.",
+        correct: true,
+      },
+      {
+        text: "A callback is a function that will be executed after another function is declared.",
+        correct: false,
+      },
+      {
+        text: "A callback is a function that will be executed before another function gets executed.",
+        correct: false,
+      },
+      {
+        text: "A callback is a function that will be executed after another function is returned.",
+        correct: false,
+      },
+    ],
+  },
+
+  {
+    question: "What is recursion in a programming language?",
+    answer: [
+      {
+        text: "Recursion is a technique to iterate over an operation by having a function call itself repeatedly until it arrives at a result.",
+        correct: true,
+      },
+      {
+        text: "Recursion is a technique to loop over an operation by having a function call itself once until it arrives at a result.",
+        correct: false,
+      },
+      {
+        text: "Recursion is a technique to iterate over an operation by having a function call itself once until it is returned.",
+        correct: false,
+      },
+      {
+        text: "Recursion is a technique to loop over an operation by having a function call itself twice until it arrives at a result.",
+        correct: false,
+      },
+    ],
+  },
 ];
 
 let currentQuestionIndex = 0;
